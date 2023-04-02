3c18ac491b7841b042cde15f761bc24e53397ca9
NishathAfrozaAJ <nishathafroza193001071@eee.ssn.edu.in>
2023-04-02 16:44:42 +0530

    TransactionModule Push1
    Tran

---
+++ EuroBankAPI/Models/RefPaymentMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EuroBankAPI.Models
+{
+    public class RefPaymentMethods
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PaymentMethodCode { get; set; } 
+        public string PaymentMethodName { get; set; }
+    }
+}
