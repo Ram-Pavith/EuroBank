1d7c3f22a4156c0ee786b13feb6304642b83525e
Mohana <mona7sekar@gmail.com>
2023-04-02 14:40:06 +0530

    Accounts Microservice Models

--- EuroBankAPI/Data/EuroBankContext.cs
+++ EuroBankAPI/Data/EuroBankContext.cs
@@ -10,28 +10,63 @@ namespace EuroBankAPI.Data
 
         //DbSet Tables For the Context
         public DbSet<UserAuth> UsersAuth { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<CustomerCreationStatus> CustomerCreationStatuses { get; set; }
+        
 
-       
-          
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.AccountId).HasName("PK_Account");
+
+                /*entity.HasOne(d => d.AccountType)
+                    .*/
+
+                entity.Property(e => e.DateCreated).HasDefaultValue(DateTime.Now);
+
+                entity.HasCheckConstraint("Balance_check", "Balance >= 0");
+
+
+            });
+
+
+            modelBuilder.Entity<AccountType>(entity =>
+            {
+                entity.HasKey(e => e.AccountTypeId).HasName("PK_Account_Type");
+
+                entity.Property(e => e.Type).HasMaxLength(7);
+            });
 
             modelBuilder.Entity<AccountCreationStatus>(entity =>
             {
-                entity.HasKey(e => e.AccountCreationStatusId).HasName("PK_Account_Creation_Status");
+                    entity.HasKey(e => e.AccountCreationStatusId).HasName("PK_Account_Creation_Status");
 
-                entity.Property(e => e.Message).HasMaxLength(50);
+                    entity.Property(e => e.Message).HasMaxLength(50);
             });
 
             modelBuilder.Entity<Statement>(entity =>
             {
                 entity.HasKey(e => e.StatementId).HasName("PK_Statement_ID");
 
+                /*account id foreign key*/
+                
                 entity.Property(e => e.Narration).HasMaxLength(35);
 
+                entity.HasCheckConstraint("Withdrawal_Check", "Withdrawal >= 0");
+          
 
             });
+
+            modelBuilder.Entity<TransactionStatus>(entity =>
+            {
+                entity.HasKey(e => e.TransactionStatusId).HasName("PK_Transaction_Status");
+
+                //account id foreign key
+
+                entity.Property(e => e.Message).HasMaxLength(35);
+            });
         }
+    
 
 
     }
--- EuroBankAPI/Models/TransactionStatus.cs
+++ EuroBankAPI/Models/TransactionStatus.cs
@@ -3,9 +3,10 @@
     public class TransactionStatus
     {
         public int TransactionStatusId { get; set; }
+        public Guid AccountId { get; set; }
         public string Message { get; set; } = string.Empty;
         public double SourceBalance { get; set; }
         public double DestinationBalance { get; set; }
-        public virtual Account AccountId { get; set; }
+        public virtual Account Account { get; set; } = null!;
     }
 }
