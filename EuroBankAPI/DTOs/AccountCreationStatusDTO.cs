7c1be1d7b021b6c58fa2308defc8bb42725efa57
Pavith <35229068+Ram-Pavith@users.noreply.github.com>
2023-04-03 18:05:17 +0530

    Accounts DTOs Created

---
+++ EuroBankAPI/DTOs/AccountCreationStatusDTO.cs
@@ -0,0 +1,17 @@
+﻿using EuroBankAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EuroBankAPI.DTOs
+{
+    public class AccountCreationStatusDTO
+    {
+        [Key]
+        public int AccountCreationStatusId { get; set; }
+
+        /*  [ForeignKey("Account")]
+          public Guid AccountId { get; set; }*/
+        public string Message { get; set; } = string.Empty;
+
+        public virtual AccountDTO Account { get; set; } = null!;
+    }
+}
---
+++ EuroBankAPI/DTOs/AccountDTO.cs
@@ -0,0 +1,35 @@
+﻿using EuroBankAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EuroBankAPI.DTOs
+{
+    public class AccountDTO
+    {
+        public AccountDTO()
+        {
+            TransactionStatuses = new HashSet<TransactionStatusDTO>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid AccountId { get; set; } = Guid.NewGuid();
+
+        public int AccountTypeId { get; set; }
+        [ForeignKey("AccountCreationStatusDTO")]
+        public int AccountCreationStatusId { get; set; }
+        public string CustomerId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public double Balance { get; set; }
+
+        public virtual AccountTypeDTO AccountType { get; set; } = null!;
+
+        public virtual CustomerDTO Customer { get; set; } = null!;
+
+        public virtual AccountCreationStatusDTO AccountCreationStatus { get; set; }
+
+        public virtual ICollection<StatementDTO> Statements { get; set; }
+
+        public virtual ICollection<TransactionStatusDTO> TransactionStatuses { get; set; }
+    }
+}
