5a53a6c9c6726866efe1c7dba2f23996bf8966bf
Pavith <35229068+Ram-Pavith@users.noreply.github.com>
2023-04-03 19:33:47 +0530

    Mapper for accounts module

--- EuroBankAPI/DTOs/Mapper.cs
+++ EuroBankAPI/DTOs/Mapper.cs
@@ -7,15 +7,24 @@ namespace EuroBankAPI.DTOs
     {
         public Mapper()
         {
+            //Employee Mapper
             CreateMap<Employee, EmployeeDTO>().ReverseMap();
+            //Customer Mapper
             CreateMap<Customer, CustomerDTO>().ReverseMap();
             CreateMap<CustomerCreationStatus, CustomerCreationStatusDTO>().ReverseMap();
+            //Transaction Mapper
             CreateMap<Transaction, TransactionDTO>().ReverseMap();
             CreateMap<RefTransactionStatus,RefTransactionStatusDTO>().ReverseMap();
             CreateMap<RefTransactionType,RefTransactionTypeDTO>().ReverseMap();
             CreateMap<Models.Service, ServiceDTO>().ReverseMap();
             CreateMap<RefPaymentMethod, RefPaymentMethodDTO>().ReverseMap();
             CreateMap<CounterParty, CounterPartyDTO>().ReverseMap();
+            //Accounts Mapper
+            CreateMap<Account,AccountDTO>().ReverseMap();
+            CreateMap<AccountType,AccountTypeDTO>().ReverseMap();   
+            CreateMap<TransactionStatus,TransactionStatusDTO>().ReverseMap();
+            CreateMap<AccountCreationStatus, AccountCreationStatusDTO>().ReverseMap();
+            CreateMap<Statement,StatementDTO>().ReverseMap();
         }
     }
 }
