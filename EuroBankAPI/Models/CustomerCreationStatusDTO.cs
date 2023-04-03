997638e91bafb037a57d1b8aa8f9ea57ee3712f5
ASHOK KUMAR <ashokkumarlm10@gmail.com>
2023-04-03 10:33:53 +0530

    summa

--- EuroBankAPI/Models/CustomerCreationStatusDTO.cs
+++ EuroBankAPI/Models/CustomerCreationStatusDTO.cs
@@ -4,10 +4,8 @@ namespace EuroBankAPI.Models
 {
     public class CustomerCreationStatusDTO
     {
-        public int Id { get; set; }
-       
-        public string CustomerId { get; set; }
-        
+        public int Id { get; set; }     
+        public string CustomerId { get; set; }    
         public string Message { get; set; }
         public virtual Customer Customer { get; set; }
 
--- EuroBankAPI/Models/CustomerDTO.cs
+++ EuroBankAPI/Models/CustomerDTO.cs
@@ -5,24 +5,14 @@ namespace EuroBankAPI.Models
     public class CustomerDTO
     {
         public string CustomerId { get; set; }
-        [DataType(DataType.EmailAddress)]
         public string EmailId { get; set; }
-
         public string Firstname { get; set; }
-
         public string Lastname { get; set; }
-
         public string Address { get; set; }
-
-        [StringLength(10, MinimumLength = 10)]
-        [Display(Name = "Phone Number")]
         public string Phone { get; set; }
         public string PasswordHash { get; set; }
         public string PasswordSalt { get; set; }
-
-        [StringLength(10, MinimumLength = 10)]
         public string PanNumber { get; set; }
-        [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
         public virtual CustomerCreationStatus CustomerCreationStatus { get; set; }
     }
