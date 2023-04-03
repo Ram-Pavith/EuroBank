629718c71830fb4a64d02116bc1709aa8ee8c97f
ASHOK KUMAR <ashokkumarlm10@gmail.com>
2023-04-03 10:32:04 +0530

    summa

---
+++ EuroBankAPI/Models/CustomerCreationStatusDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EuroBankAPI.Models
+{
+    public class CustomerCreationStatusDTO
+    {
+        public int Id { get; set; }
+       
+        public string CustomerId { get; set; }
+        
+        public string Message { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
